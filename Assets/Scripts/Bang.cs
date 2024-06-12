e8247d6bdaffa24000fcf2848dcc5a5b82fedc9d
VitalikGrinya <vitalik240899@ya.ru>
2024-06-12 11:56:13 +0300

    Update Bang.cs

--- Assets/Scripts/Bang.cs
+++ Assets/Scripts/Bang.cs
@@ -13,13 +13,11 @@ public class Bang : MonoBehaviour
     private void OnEnable()
     {
         _generate.Separated += Explode;
-        _generate.Destroyed += Explode;
     }
 
     private void OnDisable()
     {
         _generate.Separated -= Explode;
-        _generate.Destroyed -= Explode;
     }
 
     private void Explode(List<Cube> cubes)
@@ -27,24 +25,6 @@ public class Bang : MonoBehaviour
         foreach (Cube cube in cubes)
         {
             cube.AddForce(_power);
-        }
-    }
-
-    private void Explode(Cube currentCube)
-    {
-        float scale = currentCube.transform.position.x;
-        float power = _power / scale;
-        float radius = _baseRadiusOverlap / scale;
-
-        Collider[] colliders = Physics.OverlapSphere(currentCube.transform.position, radius);
-
-        foreach (var collider in colliders)
-        {
-            if (collider.gameObject.TryGetComponent(out Cube cube))
-            {
-                float distance = Vector3.Distance(currentCube.transform.position, cube.transform.position);
-                cube.AddForce(power, distance);
-            }
         }
     }
 }
